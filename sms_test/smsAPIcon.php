100997514648341ca0f4773e4042e8d3dc0552d2
ooCJoo <99072115+ooCJoo@users.noreply.github.com>
2022-02-18 04:11:59 +0800

    sms test

---
+++ sms_test/smsAPIcon.php
@@ -0,0 +1,23 @@
+<?php
+
+class smsfunction{
+        function itexmo($number,$message,$apicode,$passwd)
+        {
+
+        $ch = curl_init();
+        $itexmo = array('1' => $number, '2' => $message, '3' => $apicode, 'passwd' => $passwd);
+        curl_setopt($ch, CURLOPT_URL,"https://www.itexmo.com/php_api/api.php");
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS,http_build_query($itexmo));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        return curl_exec ($ch);
+        curl_close ($ch); 
+
+}
+}
+
+
+
+
+
+?>
