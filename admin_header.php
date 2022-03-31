693de05e4accf65e88a7e3aba39975dd0bd47d92
Tzarina Paula <80054214+goddessyui@users.noreply.github.com>
2022-04-01 05:48:51 +0800

    Update admin_header.php

--- admin_header.php
+++ admin_header.php
@@ -19,7 +19,6 @@ if ($staff_id == "" && $username == "" && $position != "Accounting Staff/Scholar
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <title>Goldenstate College of Koronadal - Admin Dashboard</title>
 </head>
