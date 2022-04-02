3fc589296f1ecf404767d17f6d13723994977baa
ooCJoo <99072115+ooCJoo@users.noreply.github.com>
2022-04-03 01:40:08 +0800

    !$db

--- dbconfig.php
+++ dbconfig.php
@@ -3,7 +3,7 @@
 
 $db = mysqli_connect("localhost", "admin", "admin", "db_appointment_system");
 
-if($db == ""){
+if(!$db){
 	echo '<script type="text/javascript">alert("Check database connection");window.location.href="404.html";</script>';
 }
 
