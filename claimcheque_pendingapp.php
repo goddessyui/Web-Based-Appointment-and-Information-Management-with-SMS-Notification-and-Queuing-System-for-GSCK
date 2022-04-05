221475e5129ad2afd7cee70ac247f64b049a22f8
Tzarina Paula <80054214+goddessyui@users.noreply.github.com>
2022-04-06 02:38:16 +0800

    Update claimcheque_pendingapp.php

--- claimcheque_pendingapp.php
+++ claimcheque_pendingapp.php
@@ -196,6 +196,9 @@
                 echo "Test: You are not an Accounting Staff/Scholarship Coordinator";
             }//if not Accounting Staff/Scholarship Coordinator
         ?>
+        <!-------------------------------Start of the BACK TO TOP BUTTON ------------------------------------>
+        <button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
+        <!-------------------------------End of the BACK TO TOP BUTTON ------------------------------------>   
     </main>
 </body>
 </html>
@@ -204,42 +207,62 @@
 
 
 <script>
-        
-            function selectAll(){
-				var items=document.getElementsByName('pending[]');
-				for(var i=0; i<items.length; i++){
-					if(items[i].type=='checkbox')
-						items[i].checked=true;
-				}
-			}
-        
-			function UnSelectAll(){
-				var items=document.getElementsByName('pending[]');
-				for(var i=0; i<items.length; i++){
-					if(items[i].type=='checkbox')
-						items[i].checked=false;
-				}
-			}
+    //-----------------START OF BUTTONS FOR ALL--------------------------------//     
+    function selectAll(){
+        var items=document.getElementsByName('pending[]');
+        for(var i=0; i<items.length; i++){
+            if(items[i].type=='checkbox')
+                items[i].checked=true;
+        }
+    }
 
-            function select10(){
-				var items=document.getElementsByName('pending[]');
+    function UnSelectAll(){
+        var items=document.getElementsByName('pending[]');
+        for(var i=0; i<items.length; i++){
+            if(items[i].type=='checkbox')
+                items[i].checked=false;
+        }
+    }
 
-                for(var i=0; i<items.length; i++){
-					if(items[i].type=='checkbox')
-						items[i].checked=false;
-				}
-				for(var i=0; i<10; i++){
-                    
-					if(items[i].type=='checkbox')
-                    
-						items[i].checked=true;
-				}
-			}
+    function select10(){
+        var items=document.getElementsByName('pending[]');
 
-           
-           
+        for(var i=0; i<items.length; i++){
+            if(items[i].type=='checkbox')
+                items[i].checked=false;
+        }
+        for(var i=0; i<10; i++){
             
+            if(items[i].type=='checkbox')
             
+                items[i].checked=true;
+        }
+    }
+    //----------------END OF BUTTONS FOR ALL-----------------------------------------------//   
+
+
+    //--------------START OF THE SCRIPT FOR THE BACK TO TOP BUTTON------------------------//
+    //Get the button:
+    mybutton = document.getElementById("myBtn");
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+    }
+
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    }
+    //--------------END OF THE SCRIPT FOR THE BACK TO TOP BUTTON------------------------//
+             
 </script>
 
 <style>
@@ -259,8 +282,27 @@
     .pending-row{
         display: flex;
         flex-wrap: wrap;
-     
     }
+
+    #myBtn { /*--------------START OF THE CSS FOR THE BACK TO TOP BUTTON------------------------*/
+        display: none; /* Hidden by default */
+        position: fixed; /* Fixed/sticky position */
+        bottom: 20px; /* Place the button at the bottom of the page */
+        right: 30px; /* Place the button 30px from the right */
+        z-index: 99; /* Make sure it does not overlap */
+        border: none; /* Remove borders */
+        outline: none; /* Remove outline */
+        background-color: red; /* Set a background color */
+        color: white; /* Text color */
+        cursor: pointer; /* Add a mouse pointer on hover */
+        padding: 15px; /* Some padding */
+        border-radius: 10px; /* Rounded corners */
+        font-size: 18px; /* Increase font size */
+    }
+
+    #myBtn:hover {
+        background-color: #555; /* Add a dark-grey background on hover */
+    } /*--------------END OF THE CSS FOR THE BACK TO TOP BUTTON------------------------*/
    
 </style>
 
