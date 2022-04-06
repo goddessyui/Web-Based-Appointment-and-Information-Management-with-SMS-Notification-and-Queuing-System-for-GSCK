334c7cf1679b964fed5a38ab39619f9aee9f684f
Tzarina Paula <80054214+goddessyui@users.noreply.github.com>
2022-04-06 18:21:01 +0800

    Update admin.php

--- admin.php
+++ admin.php
@@ -87,117 +87,10 @@
         ?>
         <!---------------Reports for Registrar------------------------------------------------->
 
-        <!--------------Start of Parent of No. of Appointment Requests (5 DIVS - active, pending, declined, cancelled, past)-------------->
-        <h3>My Data</h3>
-        <div class="sc_container">
-            <!--------------Start of No. of Active Requests-------------->
-            <div class="status_count">
-                <h5>Active Requests</h5>
-                <p>
-                    <?php
-                        $acceptedrequest="SELECT * FROM tbl_appointment_detail INNER JOIN tbl_appointment 
-                            ON tbl_appointment_detail.appointment_id = tbl_appointment.appointment_id 
-                            INNER JOIN tbl_staff_registry ON tbl_appointment.staff_id = tbl_staff_registry.staff_id 
-                            INNER JOIN tbl_student_registry ON tbl_appointment.student_id = tbl_student_registry.student_id 
-                            WHERE tbl_appointment_detail.status = 'Accepted' AND tbl_staff_registry.staff_id = '$staff_id'";
-                        $acceptedrequest_result = mysqli_query($db, $acceptedrequest);
-                        $count = mysqli_num_rows($acceptedrequest_result);
-                        echo $count;
-                    ?>
-                </p>
-            </div>
-            <!--------------End of No. of Active Requests-------------->
-            <!--------------Start of No. of Pending Requests-------------->
-            <div class="status_count">
-                <h5>Pending Requests</h5>
-                <p>
-                    <?php
-                        $pendingrequest="SELECT * FROM tbl_appointment INNER JOIN tbl_staff_registry 
-                            ON tbl_appointment.staff_id = tbl_staff_registry.staff_id 
-                            INNER JOIN tbl_student_registry ON tbl_appointment.student_id = tbl_student_registry.student_id 
-                            WHERE NOT EXISTS(SELECT * FROM tbl_appointment_detail 
-                            WHERE tbl_appointment.appointment_id = tbl_appointment_detail.appointment_id) 
-                            AND tbl_staff_registry.staff_id = '$staff_id'";
-                        $pendingrequest_result = mysqli_query($db, $pendingrequest);
-                        $count = mysqli_num_rows($pendingrequest_result);
-                        echo $count;
-                    ?>
-                </p>
-            </div>
-            <!--------------End of No. of Pending Requests-------------->
-            <!--------------Start of No. of Missed Requests-------------->
-            <div class="status_count">
-                <h5>Missed Requests</h5>
-                <p>
-                    <?php
-                        $missedrequest="SELECT * FROM tbl_appointment_detail INNER JOIN tbl_appointment 
-                            ON tbl_appointment_detail.appointment_id = tbl_appointment.appointment_id 
-                            INNER JOIN tbl_staff_registry ON tbl_appointment.staff_id = tbl_staff_registry.staff_id 
-                            INNER JOIN tbl_student_registry ON tbl_appointment.student_id = tbl_student_registry.student_id 
-                            WHERE DATE(tbl_appointment_detail.appointment_date) < CURDATE() 
-                            AND tbl_appointment_detail.status = 'Accepted' 
-                            AND tbl_staff_registry.staff_id = '$staff_id'";
-                        $missedrequest_result = mysqli_query($db, $missedrequest);
-                        $count = mysqli_num_rows($missedrequest_result);
-                        echo $count;
-                    ?>
-                </p>
-            </div>
-            <!--------------End of No. of Missed Requests-------------->
+       <?php
+        include("count_app.php");
+       ?>
 
-            <!--------------Start of No. of Declined Requests-------------->
-            <div class="status_count">
-                <h5>Declined Requests</h5>
-                <p>
-                    <?php
-                        $declinedrequest="SELECT * FROM tbl_appointment_detail INNER JOIN tbl_appointment 
-                            ON tbl_appointment_detail.appointment_id = tbl_appointment.appointment_id 
-                            INNER JOIN tbl_staff_registry ON tbl_appointment.staff_id = tbl_staff_registry.staff_id 
-                            INNER JOIN tbl_student_registry ON tbl_appointment.student_id = tbl_student_registry.student_id 
-                            WHERE tbl_appointment_detail.status = 'Declined' AND tbl_appointment.staff_id = '$staff_id'";
-                        $declinedrequest_result = mysqli_query($db, $declinedrequest);
-                        $count = mysqli_num_rows($declinedrequest_result);
-                        echo $count;
-                    ?>
-                </p>
-            </div>
-            <!--------------End of No. of Declined Requests-------------->
-            <!--------------Start of No. of Cancelled Requests-------------->
-            <div class="status_count">
-                <h5>Cancelled Requests</h5>
-                <p>
-                    <?php
-                        $cancelledrequest="SELECT * FROM tbl_appointment_detail INNER JOIN tbl_appointment 
-                            ON tbl_appointment_detail.appointment_id = tbl_appointment.appointment_id 
-                            INNER JOIN tbl_staff_registry ON tbl_appointment.staff_id = tbl_staff_registry.staff_id 
-                            INNER JOIN tbl_student_registry ON tbl_appointment.student_id = tbl_student_registry.student_id 
-                            WHERE tbl_appointment_detail.status = 'Cancelled' AND tbl_appointment.staff_id = '$staff_id'";
-                        $cancelledrequest_result = mysqli_query($db, $cancelledrequest);
-                        $count = mysqli_num_rows($cancelledrequest_result);
-                        echo $count;
-                    ?>
-                </p>
-            </div>
-            <!--------------End of No. of Cancelled Requests-------------->
-            <!--------------Start of No. of Past Requests-------------->
-            <div class="status_count">
-                <h5>Past Requests</h5>
-                <p>
-                    <?php
-                        $donerequest="SELECT * FROM tbl_appointment_detail INNER JOIN tbl_appointment 
-                            ON tbl_appointment_detail.appointment_id = tbl_appointment.appointment_id 
-                            INNER JOIN tbl_staff_registry ON tbl_appointment.staff_id = tbl_staff_registry.staff_id 
-                            INNER JOIN tbl_student_registry ON tbl_appointment.student_id = tbl_student_registry.student_id
-                            WHERE tbl_appointment_detail.status = 'done' AND tbl_appointment.staff_id = '$staff_id'";
-                        $donerequest_result = mysqli_query($db, $donerequest);
-                        $count = mysqli_num_rows($donerequest_result);
-                        echo $count;
-                    ?>
-                </p>
-            </div>
-            <!--------------End of No. of Declined Requests-------------->
-        </div>
-        <!--------------Start of Parent of No. of Appointment Requests (5 DIVS - active, pending, declined, cancelled, past)-------------->
 
         <!---------------Set Appointment Limit only seen by Registrar------------------------------------------------->
                <?php 
@@ -271,9 +164,7 @@
                 ?>
             </div>
             <div>
-                <?php
-                    if($position=="Accounting Staff/Scholarship Coordinator" OR $position=="Teacher"){
-                        ?>
+                
                             <h4>No. of Appointment Slots Taken Today:</h4>
                         <?php
                         date_default_timezone_set('Asia/Manila');                           		
@@ -284,7 +175,7 @@
                         $al = mysqli_query($db, $applimit);
                         $count = mysqli_num_rows($al);
                         echo $count;
-                    }
+                    
                 ?>
             </div>
             
