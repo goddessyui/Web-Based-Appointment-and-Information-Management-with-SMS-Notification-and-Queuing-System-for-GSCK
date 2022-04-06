d7aa8754070c88f5e18f1f605db8a3517313f503
Tzarina Paula <80054214+goddessyui@users.noreply.github.com>
2022-04-06 18:21:33 +0800

    Update student_appointment_details.php

--- student_appointment_details.php
+++ student_appointment_details.php
@@ -179,9 +179,9 @@ if (empty($_SESSION['student_id'])){
             </div>
         </div> <!------End of Appointment Status Includes ---------->
     </div><!---------End of Show Appointment Based on Status----------------------------------->
-    <!-------------------------------Start of the BACK TO TOP BUTTON ------------------------------------>
-    <button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
-    <!-------------------------------End of the BACK TO TOP BUTTON ------------------------------------->        
+    <?php
+     include("backtotop.php");
+    ?>
 </div><!--End of parent-div-->
 <style>
     .parent-div{
@@ -209,25 +209,7 @@ if (empty($_SESSION['student_id'])){
     #appdone {
         display: none;
     }
-    #myBtn { /*--------------START OF THE CSS FOR THE BACK TO TOP BUTTON------------------------*/
-        display: none; /* Hidden by default */
-        position: fixed; /* Fixed/sticky position */
-        bottom: 20px; /* Place the button at the bottom of the page */
-        right: 30px; /* Place the button 30px from the right */
-        z-index: 99; /* Make sure it does not overlap */
-        border: none; /* Remove borders */
-        outline: none; /* Remove outline */
-        background-color: red; /* Set a background color */
-        color: white; /* Text color */
-        cursor: pointer; /* Add a mouse pointer on hover */
-        padding: 15px; /* Some padding */
-        border-radius: 10px; /* Rounded corners */
-        font-size: 18px; /* Increase font size */
-    }
 
-    #myBtn:hover {
-        background-color: #555; /* Add a dark-grey background on hover */
-    } /*--------------END OF THE CSS FOR THE BACK TO TOP BUTTON------------------------*/
 </style>
 
 <script>
@@ -280,27 +262,6 @@ if (empty($_SESSION['student_id'])){
             document.getElementById('appcancelled').style.display = "none";
             document.getElementById('appdone').style.display = "block";
     }
-    //--------------START OF THE SCRIPT FOR THE BACK TO TOP BUTTON------------------------//
-    //Get the button:
-    mybutton = document.getElementById("myBtn");
-
-    // When the user scrolls down 20px from the top of the document, show the button
-    window.onscroll = function() {scrollFunction()};
-
-    function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
-    }
-    }
-
-    // When the user clicks on the button, scroll to the top of the document
-    function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-    }
-    //--------------END OF THE SCRIPT FOR THE BACK TO TOP BUTTON------------------------//
 
 </script>
 
