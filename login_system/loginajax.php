0a465889ec0b69570faf0596de2ee95099928ed2
ooCJoo <99072115+ooCJoo@users.noreply.github.com>
2022-04-17 00:48:24 +0800

    updated with has password

--- login_system/loginajax.php
+++ login_system/loginajax.php
@@ -3,17 +3,17 @@ include_once("../dbconfig.php");
 session_start();
   if ($_POST['type']==1) {
     $username = $_POST['username']; 
-    $password = $_POST['password']; 
-	$query = mysqli_query($db, "SELECT * FROM tbl_student_registry WHERE username='{$username}' AND password='{$password}'");
-	$query2 = mysqli_query($db, "SELECT * FROM tbl_staff_registry WHERE username='{$username}' AND password='{$password}'");
+	$query = mysqli_query($db, "SELECT * FROM tbl_student_registry WHERE username='{$username}'");
+	$query2 = mysqli_query($db, "SELECT * FROM tbl_staff_registry WHERE username='{$username}'");
 	
     if (mysqli_num_rows($query) == 1) {
         $row = $query->fetch_assoc();
+
+    if (password_verify($_POST['password'],$row["password"])) {
         $student_id = $row["student_id"];
         $query1 = mysqli_query($db, "SELECT * FROM tbl_student_record WHERE student_id='{$student_id}'");
         
         if (mysqli_num_rows($query1) == 1) {
-          
             session_unset();
             session_destroy();
             session_start();
@@ -23,12 +23,19 @@ session_start();
         }
         else{
             echo json_encode(array("statusCode"=>203));
-       
         }
+
 	}
+   
+    else {
+        echo json_encode(array("statusCode"=>202));
+    }
+}
 
 	else if(mysqli_num_rows($query2) == 1) {
         $row = $query2->fetch_assoc();
+
+        if (password_verify($_POST['password'], $row['password'])) {
         $staff_id = $row["staff_id"];
         $position = $row["position"];
         $query3 = mysqli_query($db, "SELECT * FROM tbl_staff_record WHERE staff_id='{$staff_id}'");
@@ -48,10 +55,14 @@ session_start();
             echo json_encode(array("statusCode"=>204));
       
         }
+        
     }
     else {
         echo json_encode(array("statusCode"=>202));
-		
+    }
+    }
+    else {
+        echo json_encode(array("statusCode"=>202));	
     }
 	}
  
