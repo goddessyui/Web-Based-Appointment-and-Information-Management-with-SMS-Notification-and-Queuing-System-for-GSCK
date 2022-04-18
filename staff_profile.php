96635132c2d7da0e16932b0d7f2a6e266b0fc577
ooCJoo <99072115+ooCJoo@users.noreply.github.com>
2022-04-19 03:46:22 +0800

    updated staff profile and added ajax

--- staff_profile.php
+++ staff_profile.php
@@ -29,7 +29,6 @@ if ($staff_id == "" && $username == ""){
 }
 ?>
 <main>
-
 <hr>
 
     <div><h1>PROFILE</h1></div>
@@ -55,30 +54,19 @@ if ($staff_id == "" && $username == ""){
         Last Name: <input type="text" name="last_name" value=<?php echo $row["last_name"]?> disabled />
     </div>
 
-    <form  method="POST" action="#">
+    <form  method="POST" action="#" id="dis">
         <div>
-            Mobile Number: <input type="tel" name="number" value="<?php echo $row["mobile_number"]?>" minlength="11" maxlength="11" autocomplete="off" required />
+            Mobile Number: <input type="tel" name="number" id="number" value="<?php echo $row["mobile_number"]?>" maxlength="11" autocomplete="off" />
         </div>
         <?php 
             if ($position == 'Registrar') {?>
         <div>
             Position: 
-            <select name="position">  
+            <select name="position" id="position">  
                 <option value="Registrar" selected>Registrar</option>  
             </select>  </div>
-        <?php 
-            }
-            else {?>
-                <div>
-                    Position: 
-                    <select name="position">  
-                        <option value="Teacher" <?php echo $row["position"]=='Teacher'?'selected':''?>>Teacher</option>  
-                        <option value="Accounting Staff/Scholarship Coordinator" <?php echo $row["position"]=='Accounting Staff/Scholarship Coordinator'?'selected':''?>>Accounting Staff/Scholarship Coordinator</option>  
-                    </select>  
-                </div>
-        <?php } ?>
-    
-        <div> Appointment Type: </div>
+
+        <div><label>Appointment Type: </label></div>
         <div>
             <input type="checkbox" name="check_list[]" value="Request Documents From Registrar" <?php echo in_array("Request Documents From Registrar", $array_type)?'checked':'';?>>
             <label> Request Documents From Registrar</label><br>
@@ -103,90 +91,221 @@ if ($staff_id == "" && $username == ""){
             <input type="checkbox" name="check_list[]" value="Presentation" <?php echo in_array("Presentation", $array_type)?'checked':'';?>>
             <label> Presentation</label><br>
         </div>
+        <?php 
+            }
+            else {?>
+                <div>
+                    Position: 
+                    <select name="position" id="position">  
+                        <option value="Teacher" <?php echo $row["position"]=='Teacher'?'selected':''?>>Teacher</option>  
+                        <option value="Accounting Staff/Scholarship Coordinator" <?php echo $row["position"]=='Accounting Staff/Scholarship Coordinator'?'selected':''?>>Accounting Staff/Scholarship Coordinator</option>  
+                    </select>  
+                </div>
+
+        <div><label>Appointment Type: </label></div>
+        <div>
+            <input type="checkbox" name="check_list[]" value="Evaluation of Grades" <?php echo in_array("Evaluation of Grades", $array_type)?'checked':'';?>>
+            <label> Evaluation of Grades - Department Head</label><br>
+            <input type="checkbox" name="check_list[]" value="UniFAST - Claim Cheque" <?php echo in_array("UniFAST - Claim Cheque", $array_type)?'checked':'';?>>
+            <label> UniFAST - Claim Cheque</label><br>
+            <input type="checkbox" name="check_list[]" value="UniFAST - Submit Documents" <?php echo in_array("UniFAST - Submit Documents", $array_type)?'checked':'';?>>
+            <label> UniFAST - Submit Documents</label><br>
+            <input type="checkbox" name="check_list[]" value="Meeting" <?php echo in_array("Meeting", $array_type)?'checked':'';?>>
+            <label> Meeting</label><br>
+            <input type="checkbox" name="check_list[]" value="Module Claiming/Submission" <?php echo in_array("Module Claiming/Submission", $array_type)?'checked':'';?>>
+            <label> Module Claiming/Submission</label><br>
+            <input type="checkbox" name="check_list[]" value="Project Submission" <?php echo in_array("Project Submission", $array_type)?'checked':'';?>>
+            <label> Project Submission</label><br>
+            <input type="checkbox" name="check_list[]" value="Presentation" <?php echo in_array("Presentation", $array_type)?'checked':'';?>>
+            <label> Presentation</label><br>
+        </div>
+        <?php } ?>
+    
+        
 
         <div>
-        <button type="submit" name="button_edit_profile">Save Changes</button>
+        <input type="button" name="button_edit_profile" value="Save Changes" id="button_edit_profile" />
         </div>
-
+        <div class="form-group">
+		<small id="message" class="" style="color:red;"></small>
+	    </div>        
     <hr>
         <div><h2>Change Password</h2></div>
         <label>Current password</label>
         <div>
-            <input type="password" name="currentpass" value="" placeholder="Current password" minlength="5" autocomplete="off" />
+            <input type="password" name="currentpass" id="currentpass" placeholder="Current password" autocomplete="off" />
         </div>
         <label>New Password</label>
         <div >
-            <input type="password" name="newpass" value="" placeholder="New password" minlength="5" autocomplete="off" />
+            <input type="password" name="newpass" id="newpass" placeholder="New password" autocomplete="off" />
         </div>
         <div class="">
-            <p>password must be at least 5 characters and<br /> have a number character, e.g. 1234567890</p>
+            <small>password must be at least 5 characters and<br /> have a number character, e.g. 1234567890</small>
         </div>
         <label>Re-enter New Password</label>
         <div>
-            <input type="password" name="newpass_verify" value="" placeholder="Re-enter new password" minlength="5" autocomplete="off" />
+            <input type="password" name="newpass_verify" id="newpass_verify" placeholder="Re-enter new password" autocomplete="off" />
 
         <div>
-            <button class="" type="submit" name="button_change_pass">Change Password</button>
+            <input type="button" name="button_change_pass" value="Save Changes" id="button_change_pass" />
         </div>
+        <div class="form-group">
+		<small id="message1" class="" style="color:red;"></small>
+	    </div>
     </form>
 
-<?php
-$staff_id = $row['staff_id'];
- if (isset($_POST['button_edit_profile'])) {
-    $new_mobilenumber = $_POST['number'];
-    $new_position = $_POST["position"];
-    $type = $_POST['check_list'];
-    $sql = "UPDATE tbl_staff_registry SET mobile_number='".$new_mobilenumber."', position='".$new_position."' WHERE staff_id = '{$staff_id}'";
-        if (mysqli_query($db, $sql)) {
-            $stmt = $db->prepare("DELETE FROM tbl_staff_appointment WHERE staff_id = '{$staff_id}'");
-	        if ($stmt->execute()){
-                foreach($type as $types){
-                    $query = "INSERT INTO tbl_staff_appointment (appointment_type, staff_id)VALUES ('{$types}', '{$staff_id}')";
-                    $query_run = mysqli_query($db, $query);
-                    
-                    $_SESSION["position"] = $new_position;
-                    }  
-        echo "Profile Settings Changes";}
-        } else {
-        echo "Error changing password:  $sql." . mysqli_error($db);
-        }
-        
-     
-    
-}
-    if (isset($_POST['button_change_pass'])) {
-        $currentpassword = $_POST['currentpass'];
-        $newpassword = $_POST['newpass'];
-        $verify_newpassword = $_POST['newpass_verify'];
-        if($currentpassword == $row['password']){
-        if (strlen($newpassword) >= 5 && strpbrk($newpassword, "1234567890") != false ){
-        if ($newpassword == $verify_newpassword){
-            $sql = "UPDATE tbl_staff_registry SET password = '".$newpassword."' WHERE staff_id = '{$staff_id}'";
-            if (mysqli_query($db, $sql)) {
-                echo "Password updated successfully";
-              } else {
-                echo "Error changing password:  $sql." . mysqli_error($db);
-              }
-            
-         }else{
-             echo "Password not Match";
-         }}
-         else{
-            echo 'Your password is not strong enough. Please use another.';
-            }
-        }else{
-            echo "Password Incorrect";
-    }
-}
 
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script>
+$(document).ready(function() {
+    // UPDATE PROFILE
+	$('#button_edit_profile').on('click', function() { 
+        $("#dis :input").prop('disabled', true);    
+		var new_mobilenumber = $('#number').val();
+		var new_position = $('#position').val();
+        var type = [];
+        $(':checkbox:checked').each(function(i){
+          type[i] = $(this).val();
+        });
+        if(new_mobilenumber!=""&&new_position!=""&&type!=""){
+        if (!/^[0-9]+$/.test(new_mobilenumber)) {
+            $("#dis :input").prop('disabled', false);   
+			$('#message').html('Phone number only a number character'); 
+		}
+		else if (new_mobilenumber.length != 11) {
+            $("#dis :input").prop('disabled', false);   
+			$('#message').html('Phone number must be at 11 characters'); 
+		}
+		else if (new_mobilenumber.substring(0, 2)!='09') {
+            $("#dis :input").prop('disabled', false);   
+			$('#message').html('Incorrect phone number !!'); 
+		}
+        else{
+			$.ajax({
+				url: "staff_profileajax.php",
+				type: "POST",
+				data: {
+					type:1,	
+					new_mobilenumber: new_mobilenumber,
+                    new_position: new_position,
+                    types:type						
+				},
+                cache: false,
+				success: function(dataResult){
+					var dataResult = JSON.parse(dataResult);
+					if(dataResult.statusCode==200){  
+                        $('#message').html('Updated profile settings sucessfully !!'); 
+                        setTimeout( function() { location.href = "staff_profile.php" }, 1000 );    
+					}
+                    else if(dataResult.statusCode==201){
+                        $("#dis :input").prop('disabled', false);   
+						$('#message').html('An Error occured, please refresh the page !!'); 
+					}
+                    else if(dataResult.statusCode==202){
+                        $("#dis :input").prop('disabled', false);   
+						$('#message').html('Mobile number already in used, please try another !!'); 
+					}
+					
+				}
+			});
+        }
+    }
+    else{
+        $("#dis :input").prop('disabled', false);   
+		$('#message').html('Please fill all the field !!'); 
+    }
+		
+	});
+	// UPDATE PROFILE
+
+
+    // CHANGE PASS
+    $('#button_change_pass').on('click', function() {
+        $("#dis :input").prop('disabled', true);  
+        var currentpass = $('#currentpass').val();
+		var newpass = $('#newpass').val();
+		var newpass_verify = $('#newpass_verify').val();
+        if(currentpass!=""&&newpass!=""&&newpass_verify!=""){
+        if (newpass.length < 8) {
+            $("#dis :input").prop('disabled', false); 
+      	$("#message1").html('Password must be at least 8 characters !!');
+        }
+      	else if (newpass.length > 16) {
+            $("#dis :input").prop('disabled', false);
+        $("#message1").html('Password must not exceed 16 characters !!'); 
+        }
+		else if (!/^(?!.* )/.test(newpass)) {
+            $("#dis :input").prop('disabled', false);
+		$('#message1').html('Password must not contain space !!'); 
+		}
+        else if  (newpass.search(/[a-z]/i) < 0) {
+            $("#dis :input").prop('disabled', false);
+        $("#message1").html('Password must contain at least one letter !!');
+        }
+        else if  (newpass.search(/[0-9]/) < 0) {
+            $("#dis :input").prop('disabled', false);
+        $("#message1").html('Password must contain at least one digit !!'); 
+        }
+      	else if (newpass != newpass_verify){
+            $("#dis :input").prop('disabled', false);
+            $('#message1').html('New Password did not match !!'); 
+      	}
+            else{
+			$.ajax({
+				url: "staff_profileajax.php",
+				type: "POST",
+				data: {
+					type:2,	
+					currentpass: currentpass,	
+					newpass: newpass					
+				},
+                cache: false,
+				success: function(dataResult){
+                  
+					var dataResult = JSON.parse(dataResult);
+					if(dataResult.statusCode==200){
+                        $("#dis :input").prop('disabled', false);
+                        $('#currentpass').val('');
+                        $('#newpass').val('');
+                        $('#newpass_verify').val('');
+                        $('#message1').html('Password updated successfully !!'); 
+					}
+                    else if(dataResult.statusCode==201){
+                        $("#dis :input").prop('disabled', false);
+						$('#message1').html('An Error occured while changing password, please refresh the page !!'); 
+					}
+                    else if(dataResult.statusCode==202){
+                        $$("#dis :input").prop('disabled', false);
+						$('#message1').html('Current password did not match !!'); 
+					}
+					
+				}
+			});
+        }
+        }
+        else{
+            $("#dis :input").prop('disabled', false);
+            $('#message1').html('Please fill all the field !!'); 
+        }
+		
+	});
+	// CHANGE PASS
+
+
+});
+</script>
+
+
+
+<?php
  include("backtotop.php");
 ?>
 
 </main>
 
 
-</body>
-</html>
 <style>
     main {
         margin-left: 5%;
---
+++ staff_profileajax.php
@@ -0,0 +1,91 @@
+<?php
+ include_once("dbconfig.php");
+ session_start();
+ $staff_id = $_SESSION["staff_id"];
+
+ if ($_POST['type']==1) {
+    $new_mobilenumber = $_POST['new_mobilenumber'];
+    $new_position = $_POST["new_position"];
+    $type = $_POST['types'];
+
+    $query = mysqli_query($db, "SELECT password FROM tbl_staff_registry WHERE staff_id ='{$staff_id}' AND mobile_number='{$new_mobilenumber}'");
+    if (mysqli_num_rows($query) == 1){
+    $sql = "UPDATE tbl_staff_registry SET mobile_number='".$new_mobilenumber."', position='".$new_position."' WHERE staff_id = '{$staff_id}'";
+        if (mysqli_query($db, $sql)) {
+            $stmt = $db->prepare("DELETE FROM tbl_staff_appointment WHERE staff_id = '{$staff_id}'");
+	        if ($stmt->execute()){ 
+                foreach($type as $types){
+                    $query = "INSERT INTO tbl_staff_appointment (appointment_type, staff_id)VALUES ('{$types}', '{$staff_id}')";
+                    $query_run = mysqli_query($db, $query);
+                    } 
+                    $_SESSION["position"] = $new_position;
+                    echo json_encode(array("statusCode"=>200));
+                } 
+                else{
+                    echo json_encode(array("statusCode"=>201)); 
+                }
+        } 
+        else {
+        echo json_encode(array("statusCode"=>201));
+        }
+
+    }
+
+        else {
+            $query1 = mysqli_query($db, "SELECT * FROM tbl_staff_registry WHERE mobile_number='{$new_mobilenumber}'");
+            $query2 = mysqli_query($db, "SELECT * FROM tbl_student_registry WHERE mobile_number='{$new_mobilenumber}'");
+            if(mysqli_num_rows($query1) == 0&&mysqli_num_rows($query2) == 0){
+                
+                $sql = "UPDATE tbl_staff_registry SET mobile_number='".$new_mobilenumber."', position='".$new_position."' WHERE staff_id = '{$staff_id}'";
+                if (mysqli_query($db, $sql)) {
+                    $stmt = $db->prepare("DELETE FROM tbl_staff_appointment WHERE staff_id = '{$staff_id}'");
+	            if ($stmt->execute()){ 
+                foreach($type as $types){
+                    $query = "INSERT INTO tbl_staff_appointment (appointment_type, staff_id)VALUES ('{$types}', '{$staff_id}')";
+                    $query_run = mysqli_query($db, $query);
+                    } 
+                    $_SESSION["position"] = $new_position;
+                    echo json_encode(array("statusCode"=>200));
+                } 
+                else{
+                    echo json_encode(array("statusCode"=>201)); 
+                }
+                } 
+                else {
+                    echo json_encode(array("statusCode"=>201));
+                }
+
+
+
+            }
+            else{
+                echo json_encode(array("statusCode"=>202));
+            }
+        }
+        
+     
+    
+}
+    if ($_POST['type']==2) {
+        $currentpassword = $_POST['currentpass'];
+        $newpassword  = password_hash($_POST['newpass'], PASSWORD_DEFAULT);
+        $query = mysqli_query($db, "SELECT password FROM tbl_staff_registry WHERE staff_id ='{$staff_id}'");
+        $row = $query->fetch_assoc();
+        if (password_verify($currentpassword,$row["password"])) {
+            $sql = "UPDATE tbl_staff_registry SET password = '".$newpassword."' WHERE staff_id = '{$staff_id}'";
+            if (mysqli_query($db, $sql)) {
+               
+                echo json_encode(array("statusCode"=>200));
+              } else {
+                echo json_encode(array("statusCode"=>201));
+                
+              }
+            
+         }else{
+           
+             echo json_encode(array("statusCode"=>202));
+         }
+        
+}
+
+?>
