4cf44342595b1f857c601a5f53b49118592fac5b
Tzarina Paula <80054214+goddessyui@users.noreply.github.com>
2022-04-24 05:48:09 +0800

    moved css to admin css

--- admin_header.php
+++ admin_header.php
@@ -18,6 +18,7 @@ if ($staff_id == ""){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="css/admin_style.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	
