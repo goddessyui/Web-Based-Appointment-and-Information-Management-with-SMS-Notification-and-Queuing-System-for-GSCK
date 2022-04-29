1a210578018f49a4bdd37d30dc357e9b4295639f
Tzarina Paula <80054214+goddessyui@users.noreply.github.com>
2022-04-29 08:56:11 +0800

    Update admin.php

--- admin.php
+++ admin.php
@@ -315,9 +315,10 @@
             }
             ?> <!--------------------- Appointment Limit and Show List of Students and Staff, only seen by Accounting Staff------------------------------------------>
 
-         
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.187416904765!2d124.83928635046632!3d6.4979414952765255!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x32f8188deb24eeb5%3A0x2b58bb78e9ae72!2sGoldenstate%20College%20of%20Koronadal%20City!5e0!3m2!1sen!2sph!4v1651155144915!5m2!1sen!2sph" width="100%" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>  
-    </main>
+    <?php
+        include("backtotop.php");
+    ?> 
+  </main>
 </body>
 </html>
 
