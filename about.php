de5c2f23c3c8ea8a34c28e4a25f342251e2dae13
Tzarina Paula <80054214+goddessyui@users.noreply.github.com>
2022-05-09 04:21:13 +0800

    about

--- about.php
+++ about.php
@@ -2,8 +2,125 @@
 include("header.php");
 ?>
 <div class="parent-div">
-    This is the about page. This is where we place other information about GSCK.
+
+    <div class="about_section">
+        <h3>
+            History of Goldenstate College
+        </h3>
+        <p>
+            &emsp; Demand for technical skills by the economic sector was growing. The Goldenstate Institute, aggressive to respond to
+            this need, offered short-term industrial courses.
+        </p>
+        <p>
+            &emsp; When it opened, the institution was manned by only seven administrative officials, twenty-nine instructors, 
+            and 160 students. Five years later, the administrative staff and instructors increased to forty-eight. Their efforts 
+            and expertise were dedicated to meet the needs of the increasing student population.
+        </p>
+        <p>
+            &emsp; In the year 1997, the Goldenstate Institute occupied two building spaces which became the venues for academic training, and 
+            specific practicum and classroom instructions. The institution started its operation with only 7 vocational courses 
+            and 5 non-formal courses. The same courses were offered with two more additional courses: Computer Secretarial and
+            Architectural Drafting.
+        </p>
+        <p>
+            &emsp; With the increasing demand for supply of technically-skilled men and women here and abroad, Goldenstate continues to 
+            upgrade and enhance its training program proficiency in order to produce graduates who can be productive and efficient 
+            in the professional world. This, along with fulfilling its educational mission in nation building, 
+            is the guiding philosophy of the Goldenstate Institute as it envisions a society where constituents live fruitfully and
+            productively within the context of an economically productive society. In the year 2001, the Board of Trustees decided to
+            change its organization's name from Goldenstate Institute, Inc to Goldenstate College, Inc and applied degree courses
+            from the Commission of Higher Education. Fortunately, the school was granted permit to operate Bachelor of Science in Tourism,
+            Bachelor of Science in Hotel and Restaurant Management, and Bachelor of Science Commerce, Major in Business Management.
+            In 2006, the Goldenstate College was fortunately granted permit to operate Bachelor of Science in Business 
+            Administration and BS Tourism.
+        </p>
+        <p>
+            &emsp; Currently, the Goldenstate College of Koronadal offers a variety of courses including BSHM, BSTM, BSIT, BSSW, ABE, 
+            BECE, BTVED, BSBA, ACT, HM, and TESDA PROGRAMs.
+        </p>
+    </div>
+
+    <div class="about_section">
+        <h3>
+            Vision
+        </h3>
+        <p>
+            Goldenstate College seeks to transform lives, organizations and communities through instruction, research
+            and community service thereby paving the way for a holistic development of productive, successful, and 
+            globally competitive professionals and graduates and responsible members of the community.
+        </p>
+        <p>
+            The VISION of GOLDENSTATE COLLEGE which is the central inspiration of the organization is: 
+            An economically, socially, and morally developed human resource who can respond to the needs of the 
+            society locally, nationally, and globally. It is committed to giving the youth access to quality education 
+            whereby students are prepared to make better decisions in life, provided with fitting opportunities to advance, 
+            develop and benefit themselves and the society.
+        </p>
+    </div>
+
+    <div class="about_section">
+        <h3>
+            Mission
+        </h3>
+        <p>
+            GOLDENSTATE COLLEGE is a Filipino, non-sectarian, educational institution established to serve the educational 
+            development needs of the society, locally, nationally, and globally.
+        </p>
+        <p>
+            We are committed to providing holistic education to our students to ensure their development as a responsible 
+            and globally competitive professional in their chosen fields of specialization enabling them to become responsible 
+            community leaders sensitive to global and national development issues and challenges. To do this, we make proactive 
+            decisions to support the career and personal development of our learners and ensure that our curriculum implementation, 
+            teaching and learning processes and school services demonstrate that we are passionate about our mission critical fundamentals:
+            <ul>
+                <li>
+                    As a college, we seek to understand and communicate the Truth by inculcating it among our students and stakeholders
+                    in all their personal, school-related activities, social activism and professional pursuits.
+                </li>
+                <li>
+                    As a Filipino International HEI, amidst the rising challenges and opportunities of globalization and ASEAN 
+                    integration, we seek to preserve and promote the Filipino culture by embedding in our curricular offerings, 
+                    research and co-curricular activities a sense of passionate understanding of Filipino identity equipped with 
+                    needed competencies required to work in a borderless world.
+                </li>
+                <li>
+                    As a Centre for Learning, we are committed to excellence in instruction, scholarly research and responsive 
+                    community service.
+                </li>
+            </ul>
+        </p>
+    </div>
+
+    <div class="about_section">
+        <h3>
+            Core Values
+        </h3>
+        <p>
+            <h4>Teamwork.</h4> Providing support to one another, working co-operatively, respecting one another's views, and
+            making our work environment fun and enjoyable.
+        </p>
+        <p>
+            <h4>Excellence.</h4> Always doing what we say we will and striving for excellence and quality in everything we do.
+        </p>
+        <p>
+            <h4>Professionalism.</h4> At all times we act with integrity, providing quality service, being reliable and responsible.
+        </p>
+        <p>
+            <h4>Honesty.</h4> Being open and honest in all our dealings and maintaining the highest integrity at all times.
+        </p>    
+    </div>
+
+    <div class="about_section">
+        <img src="image/goldenstate-iso.png" width="300">
+    </div>
+
+
 </div>
+<?php
+     include("backtotop.php");
+?>
+
+
 <style>
     .parent-div{
         padding-top: 150px;
