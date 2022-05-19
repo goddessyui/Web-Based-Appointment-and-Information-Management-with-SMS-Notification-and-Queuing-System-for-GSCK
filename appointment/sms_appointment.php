fa9bca94e845ba1c4ab918ec6df5991af2c3eca8
ooCJoo <99072115+ooCJoo@users.noreply.github.com>
2022-05-20 05:47:26 +0800

    updated message content

--- appointment/sms_appointment.php
+++ appointment/sms_appointment.php
@@ -2,8 +2,9 @@
 if (isset($accept)){
     include '../sms_test/smsAPIcon.php';
     $receiver = $m_number;
-    $message = $student_fullname.":\n\n". $fullnames . ", has accepted your request for ".$appointment_type." and is set on " . date("F d, Y", strtotime($appointment_date)).
-    ". Your queue number is ".$queuenumber."\n\n -Goldenstate College";
+    $message = $student_fullname.":\n\n". $fullnames . ", has accepted your request for ".$appointment_type." and is set on " 
+    . date("F d, Y", strtotime($appointment_date)).' from '. date("h:ia", strtotime($appointment_time_open)).' - '. date("h:ia", strtotime($appointment_time_close)).
+    " only. Your unique queue number is ".$queuenumber."\n\n -Goldenstate College";
     $send = new smsfunction();
     $send->itexmo($receiver, $message, $smsAPICode, $smsAPIPassword);
 
@@ -21,7 +22,7 @@ else if (isset($decline)){
 else if (isset($move)){
     include '../sms_test/smsAPIcon.php';
     $receiver = $m_number;
-    $message = $student_fullname.":\n\n". $fullnames . ", has rescheduled the appointment for ".$appointment_type." the new date is on " . date("F d, Y", strtotime($newDate)).". Your new queue number is ".$queuenumber."\n\n -Goldenstate College";
+    $message = $student_fullname.":\n\n". $fullnames . ", has rescheduled the appointment for ".$appointment_type.". Your new schedule is set on " . date("F d, Y", strtotime($newDate)).' from '. date("h:ia", strtotime($appointment_time_open)).' - '. date("h:ia", strtotime($appointment_time_close))." only. Your new unique queue number is ".$queuenumber."\n\n -Goldenstate College";
     $send = new smsfunction();
     $send->itexmo($receiver, $message, $smsAPICode, $smsAPIPassword);
 }
--- appointment/sms_unifast.php
+++ appointment/sms_unifast.php
@@ -2,13 +2,13 @@
 if (isset($accept)){
     $receiver = $m_number;
     $message = $student_fullname.":\n\n". $fullnames . ", has accepted your request for ".$appointment_type." and is set on " . date("F d, Y", strtotime($appointment_date)).
-    ". Your queue number is ".$queuenumber."\n\n -Goldenstate College";
+    " from 8AM - 5PM only. Your unique queue number is ".$queuenumber."\n\n -Goldenstate College";
     $send->itexmo($receiver, $message, $smsAPICode, $smsAPIPassword);
 
 }
 else if (isset($accept_unifast)){
     $receiver = $m_number;
-    $message =  $student_fullname.":\n\nCongratulations! You are a TES grantee, your schedule is on " . date("F d, Y", strtotime($appointment_date)).". Your queue number is ".$queuenumber."\n\n -Goldenstate College";;
+    $message =  $student_fullname.":\n\nCongratulations! You are a TES grantee, your schedule is on " . date("F d, Y", strtotime($appointment_date))." from 8AM - 5PM only. Your unique queue number is ".$queuenumber."\n\n -Goldenstate College";;
     $send->itexmo($receiver, $message, $smsAPICode, $smsAPIPassword);
 
 }
