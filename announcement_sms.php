158482af400081ac0513de7c3ed8ce5b71fc2edd
ooCJoo <99072115+ooCJoo@users.noreply.github.com>
2022-05-19 22:36:34 +0800

    filter sms message to only enrolled student

--- announcement_sms.php
+++ announcement_sms.php
@@ -33,7 +33,8 @@ switch ($year) {
 
 if ($course=='ALL' && $year=='ALL'){
     $sql_sms = "SELECT
-    mobile_number
+    mobile_number,
+    staff_id
     FROM
     tbl_student_registry";
 
@@ -41,9 +42,12 @@ if ($course=='ALL' && $year=='ALL'){
     if (mysqli_num_rows($res_sms) > 0) {
 
     while ($row_sms = mysqli_fetch_assoc($res_sms)) {
+        $check = mysqli_query($db, "SELECT staff_id FROM tbl_staff_record WHERE staff_id='{$row_sms('staff_id')}'");
+        if (mysqli_num_rows($check)!=1){
         $receiver = $row_sms['mobile_number'];
         $message = "Hello, Good day!\n\n". $fullnames . ", added new announcement in gsck.online/announcements.php \n\n -Goldenstate College";
         $send->itexmo($receiver, $message, $smsAPICode, $smsAPIPassword);
+        }
         
     }
 
@@ -53,7 +57,8 @@ if ($course=='ALL' && $year=='ALL'){
 
 else if ($course=='ALL' && $year!='ALL'){
     $sql_sms = "SELECT
-    mobile_number
+    mobile_number,
+    staff_id
     FROM
     tbl_student_registry
     WHERE `year` = '$year'";
@@ -61,9 +66,12 @@ else if ($course=='ALL' && $year!='ALL'){
     if (mysqli_num_rows($res_sms) > 0) {
 
     while ($row_sms = mysqli_fetch_assoc($res_sms)) {
+        $check = mysqli_query($db, "SELECT staff_id FROM tbl_staff_record WHERE staff_id='{$row_sms('staff_id')}'");
+        if (mysqli_num_rows($check)!=1){
         $receiver = $row_sms['mobile_number'];
         $message = "Hello, Good day!\n\n". $fullnames . ", added new announcement specifically for " . $year_lvl . " year student in gsck.online/announcements.php \n\n -Goldenstate College";
         $send->itexmo($receiver, $message, $smsAPICode, $smsAPIPassword);
+        }
       
     }
 
@@ -73,7 +81,8 @@ else if ($course=='ALL' && $year!='ALL'){
 
 else if ($course!='ALL' && $year=='ALL'){
     $sql_sms = "SELECT
-    mobile_number
+    mobile_number,
+    staff_id
     FROM
     tbl_student_registry
     WHERE course = '$course'";
@@ -81,9 +90,12 @@ else if ($course!='ALL' && $year=='ALL'){
     if (mysqli_num_rows($res_sms) > 0) {
 
     while ($row_sms = mysqli_fetch_assoc($res_sms)) {
+        $check = mysqli_query($db, "SELECT staff_id FROM tbl_staff_record WHERE staff_id='{$row_sms('staff_id')}'");
+        if (mysqli_num_rows($check)!=1){
         $receiver = $row_sms['mobile_number'];
         $message = "Hello, Good day!\n\n". $fullnames . ", added new announcement specifically for " . $course . " student in gsck.online/announcements.php \n\n -Goldenstate College";
         $send->itexmo($receiver, $message, $smsAPICode, $smsAPIPassword);
+        }
       
     }
 
@@ -93,7 +105,8 @@ else if ($course!='ALL' && $year=='ALL'){
 
 else {
     $sql_sms = "SELECT
-    mobile_number
+    mobile_number,
+    staff_id
     FROM
     tbl_student_registry
     WHERE `year` = '$year' AND course = '$course'";
@@ -101,9 +114,12 @@ else {
     if (mysqli_num_rows($res_sms) > 0) {
 
     while ($row_sms = mysqli_fetch_assoc($res_sms)) {
+        $check = mysqli_query($db, "SELECT staff_id FROM tbl_staff_record WHERE staff_id='{$row_sms('staff_id')}'");
+        if (mysqli_num_rows($check)!=1){
         $receiver = $row_sms['mobile_number'];
         $message = "Hello, Good day!\n\n". $fullnames . ", added new announcement specifically for ". $year_lvl ." year " . $course . " student in gsck.online/announcements.php \n\n -Goldenstate College";
         $send->itexmo($receiver, $message, $smsAPICode, $smsAPIPassword);
+        }
       
     }
 
